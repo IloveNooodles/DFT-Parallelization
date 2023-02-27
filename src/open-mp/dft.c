84b26841db041213f8cf21d25f2265eabb6dd22f
Adiyansa Prasetya Wicaksana <adiyansawicaksana@gmail.com>
2023-02-27 17:01:19 +0700

    feat: naive dft for openmp

--- src/open-mp/dft.c
+++ src/open-mp/dft.c
@@ -1,17 +1,79 @@
 // gcc mp.c --openmp -o mp
-
 #include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <complex.h>
+#include <math.h>
 
-int main(void)
+#define MAX_N 512
+
+struct Matrix
 {
-#pragma omp parallel num_threads(8)
-  {
-    int nthreads, tid;
-    nthreads = omp_get_num_threads();
-    tid = omp_get_thread_num();
-    printf("Hello world from from thread %d out of %d threads\n", tid, nthreads);
+  int size;
+  double mat[MAX_N][MAX_N];
+};
+
+struct FreqMatrix
+{
+  int size;
+  double complex mat[MAX_N][MAX_N];
+};
+
+int readMatrix(int *n, struct Matrix *m)
+{
+  scanf("%d", &(m->size));
+  for (int i = 0; i < m->size; i++){
+    for (int j = 0; j < m->size; j++){
+      scanf("%lf", &(m->mat[i][j]));
+    }
   }
-  return 0;
+
+  return m->size;
+}
+
+double complex dft(struct Matrix *mat, int k, int l)
+{
+  double complex element = 0.0;
+  for (int m = 0; m < mat->size; m++)
+  {
+    for (int n = 0; n < mat->size; n++)
+    {
+      double complex arg = (k * m / (double)mat->size) + (l * n / (double)mat->size);
+      double complex exponent = cexp(-2.0I * M_PI * arg);
+      element += mat->mat[m][n] * exponent;
+    }
+  }
+  return element / (double)(mat->size * mat->size);
+}
+
+int main(void) 
+{
+  struct Matrix source;
+  struct FreqMatrix freq;
+  
+  int n, k, l;
+  double elapsed;
+  double complex sum = 0;
+
+  double start, finish;
+
+  n = readMatrix(&n, &source);
+
+  start = omp_get_wtime();
+
+  #pragma omp parallel shared(source) num_threads(8)
+  {
+    #pragma omp for collapse(2) reduction(+:sum)
+    for (k = 0; k < source.size; k++){
+      for (l = 0; l < source.size; l++){
+        sum += dft(&source, k, l);
+      }
+    }
+  }
+
+  finish = omp_get_wtime();
+
+  sum /= source.size;
+  printf("Elapsed time: %lf seconds\n", finish-start);
+  printf("Average : (%lf, %lf)", creal(sum), cimag(sum));
 }
